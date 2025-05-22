b86ac207523e8c262742be7ad5c262aedb5a94b8
Oleksii <sukhenko.oleksii@gmail.com>
2025-05-22 09:22:18 +0300

    start booking-app

---
+++ src/test/java/mate/academy/booking_app/BookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.booking_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
